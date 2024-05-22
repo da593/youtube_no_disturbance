c41938906de333a9d20410876e3480b7e7d5079b
da593 <dylanau99@gmail.com>
2024-05-21 22:22:30 -0400

    correctly find video player and channel info from DOM

--- src/content_script.ts
+++ src/content_script.ts
@@ -10,7 +10,6 @@ let mainVideo: HTMLVideoElement  //video always available if movie_player is ava
 let skipButton: HTMLInputElement | null  //when ad is showing
 let confirmButton: HTMLElement | null  //when video is paused
 
-
 let videoObserver: MutationObserver;
 
 browser.storage.sync.get().then((items) => {
@@ -27,7 +26,7 @@ browser.storage.onChanged.addListener((changes, namespace) => {
             whitelist = convertStringToSet(newValue);
         } 
         else {
-            userOptions[key] = newValue
+            userOptions[key] = newValue;
         }
     }
     if (!userOptions["mute_ad"] && mainVideo ) {
@@ -69,19 +68,43 @@ function handlePause() {
 function handleAd() {
     if (video_player_container) {
         if (userOptions["skip_ad"] || userOptions["mute_ad"]) {
+
             const isAd: boolean = video_player_container.classList.contains("ad") 
                                 || video_player_container.classList.contains("ad-showing") 
                                 || video_player_container.classList.contains("ad-interrupting");
+
             if (isAd) {
+
                 if (userOptions["mute_ad"]) {
                     mainVideo.muted = true;
                 }
+
                 if (userOptions["skip_ad"]) {
-                    const channel_info: Element | null | undefined = document.getElementById('channel-info')?.querySelector("#text.style-scope.ytd-channel-name");
+
+                    let channel_info: Element | null | undefined = document.querySelector("#below .watch-active-metadata #text-container a");
+
+                    if (channel_info === null || channel_info === undefined) {
+                        let startTime = new Date().getTime();
+
+                        const id = setInterval(() => {
+                            if (new Date().getTime() - startTime > 5000 || channel_info) {
+                                clearInterval(id);
+                            }
+                            if (channel_info === null || channel_info === undefined) {
+                                channel_info = document.querySelector("#below .watch-active-metadata #text-container a");
+                                clearInterval(id);
+                            }
+                        })
+                    }
+
                     if (channel_info) {
-                        const channel_name: string | null = channel_info.textContent;
+
+                        const channel_name: string | null = channel_info.innerHTML;
+
                         if (channel_name && !whitelist.has(channel_name)) {
-                            const skipButton: HTMLInputElement | null = document.querySelector('button[class*="ytp-ad-skip-button"') as HTMLInputElement;
+
+                            const skipButton: HTMLInputElement | null = document.querySelector('button[class*="ytp-skip-ad-button"]') as HTMLInputElement;
+                            
                             if (skipButton) {
                                 skipButton.click();
                             }
@@ -104,7 +127,7 @@ function observeForVideoPlayer() {
     new MutationObserver(function(mutations: MutationRecord[], observer: MutationObserver) {
         video_player_container = document.getElementById("movie_player");
         if (video_player_container) {
-            mainVideo = document.querySelector('video[class*="html5-main-video"]') as HTMLVideoElement;
+            mainVideo = document.querySelector('#movie_player video[class*="html5-main-video"]') as HTMLVideoElement;
             if (mainVideo) {
                 observer.disconnect();
                 videoPlaying();
